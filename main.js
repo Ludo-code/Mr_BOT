703aa89f3a25f2a359abe2c87c9870a3894776ec
Ludo-code <lasagne.berthet@gmail.com>
2019-10-23 18:34:18 +0200

    petite maj du bot (securite & prefix)

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const { Client } = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
 const client = new Client({ disableEveryone: true });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Bot prêt au combats !!!");
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") msg.channel.send("Pong");
-  if (msg.content === "everyone")
-    msg.channel.send("@everyone, salut a tous !!!", { disableEveryone: false });
-  if (msg.content === "noteveryone")
-    msg.channel.send("@everyone (noteveryone), salut a tous !!!");
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("Ping");
 });
 
-client.login("NjM2MDk4OTEyODU5NzE3NjM0.Xa-Clw.ERBrQuPwIBGAoXROyhzjsGzarLA");
+client.login(TOKEN);
