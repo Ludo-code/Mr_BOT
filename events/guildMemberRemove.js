113631c7f688c724a7e101dd3cf3d549a9b9d0b0
Ludo-code <lasagne.berthet@gmail.com>
2019-12-13 21:00:03 +0100

    Correction de bug et ajout de commande.

--- events/guildMemberRemove.js
+++
@@ -1,4 +0,0 @@
-module.exports = (client, member) => {
-  const channel = client.channels.find(r => r.name === "entrée-sortie");
-  channel.send(`${member} nous a quitté, nous le regrettons déja !`);
-};
