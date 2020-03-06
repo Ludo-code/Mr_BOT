06875b8cdadedc8f6cf374deea82c2f3319a78e7
Ludo-code <lasagne.berthet@gmail.com>
2020-03-06 20:21:22 +0100

    test

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.TOKEN = "NjM2MDk4OTEyODU5NzE3NjM0.XmKfDg.hnPKN8_73NjQxQRTCbzLqMEZOmI";
+exports.TOKEN = "NjM2MDk4OTEyODU5NzE3NjM0.XmKhDw.iK6WO0D3gB9MeAwzvWIOqUtbo4o";
 exports.PREFIX = "@";
 exports.DBCONNECTION = "mongodb://localhost:27017/discordbottest";
