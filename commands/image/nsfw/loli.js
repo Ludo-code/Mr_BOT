5b4c8df897073493b4fcbf536a14a3cd32f6433e
Ludo-code <lasagne.berthet@gmail.com>
2020-03-06 19:59:27 +0100

    hey correction

--- commands/image/nsfw/loli.js
+++ commands/image/nsfw/loli.js
@@ -1,10 +1,25 @@
 module.exports = async (client, message) => {
-  if (message.channel.type === "dm") return message.reply("Alors on essaye de se cacher ! :joy:");
+  if (message.channel.type === "dm") return message.reply("Je veux bien du loli mais va dans un channel nsfw ! :joy:");
+  if (!message.channel.nsfw) return message.channel.send(`Désolé ${message.author} mais tu ne peux faire cette commande que dans un salon nsfw !`);
+  const calcul_age = [
+    "12",
+    "45",
+    "123",
+    "26",
+    "87",
+    "97",
+    "28",
+    "84",
+    "69",
+    "900"
+  ];
+  const age =
+    calcul_age[Math.floor(Math.random() * calcul_age.length)];
   const phrase_rdm_yes = [
-    "test 1",
-    "test 2",
-    "test 3",
-    "test 4"
+    "Attend tu m'as demandé une image de loli surtout ne bouge pas j'appel la police :joy:",
+    "Du loli du loli laisse moi réfléchir non désolé mais je ne peux pas accéder a ta requête comme c'est dommage :joy:",
+    `J'ai une loli mais tu dois attendre encore ${age} années avant qu'elle soit assez mature c'est nos norme de maturité :joy:`,
+    "Oh bah du loli c'est pas bien mon bon monsieur pas les enfants."
   ];
   const envoie =
     phrase_rdm_yes[Math.floor(Math.random() * phrase_rdm_yes.length)];
--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.TOKEN = "NjM2MDk4OTEyODU5NzE3NjM0.XbB4Bw.s6UTZt013k5sduPCIWjVxn4MEv4";
+exports.TOKEN = "NjM2MDk4OTEyODU5NzE3NjM0.XmKRxg.71-k3jez7mUxR3KjdH2M9DDIj-c";
 exports.PREFIX = "@";
 exports.DBCONNECTION = "mongodb://localhost:27017/discordbottest";
