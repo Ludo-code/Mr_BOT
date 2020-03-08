acdced4d945984f99cc47da9de47604cd57e7f48
Ludo-code <lasagne.berthet@gmail.com>
2020-03-08 07:59:56 +0100

    correction de bug et test

--- commands/gif/chat.js
+++ commands/gif/chat.js
@@ -5,8 +5,7 @@ module.exports = async (client, message) => {
   if (message.channel.type === "dm") return message.reply("Bon j'avoue tu veux un chat mais je suis désolé même cette commande je la bloque en MP ! :joy:");
   message
     .delete({ timeout: 3000 })
-    .then(console.log("Un message a été supprimé !"))
-    .then(message.channel.send(`Désolé a toi ${message.author} mais l'api que j'utilise est down donc pas de chat j'en suis désolé liens vers l'api : http://aws.random.cat/meow encore désolé pour le problème j'éspère qu'il seras vite réglé !!! `));
+    .then(console.log(`La commande neko_gif a été exécuté par ${message.author.username} de l'id : ${message.author}`));
   const chat = await fetch("http://aws.random.cat/meow")
     .then(res => res.json())
     .then(json => json.file);
