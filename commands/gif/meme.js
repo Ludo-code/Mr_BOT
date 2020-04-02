c50dddd6260844319095ed85c182c4b7d89f56fa
Ludo-code <lasagne.berthet@gmail.com>
2020-04-02 10:56:04 +0200

    upate de discord.js

--- commands/gif/meme.js
+++ commands/gif/meme.js
@@ -5,7 +5,7 @@ module.exports = async (client, message) => {
   if (message.channel.type === "dm") return message.reply("Un meme ça a rien de méchant mais je la bloque quand même dans mes MP désolé ! :joy:");
   message
     .delete({ timeout: 3000 })
-    .then(console.log("Un message a été supprimé !"));
+    .then(console.log(`La commande meme a été exécuté par ${message.author.username} de l'id : ${message.author}`));
   const meme = await fetch("https://meme-api.herokuapp.com/gimme")
     .then(res => res.json())
     .then(json => json.url);
