24745e16881cd657885686c4d593e9fe4f8858ec
Ludo-code <lasagne.berthet@gmail.com>
2020-10-03 15:25:45 +0200

    correction de fonctionnalité

--- commands/moderation/utilitaire/idée.js
+++ commands/moderation/utilitaire/idée.js
@@ -19,7 +19,7 @@ class ideecommands extends Command {
     if (message.channel.type === "dm") return message.reply("Commande bloqué en mp désolé, toute mes commandes le sont en mp !");
     const msgcontent = args.messagecontent;
     if (message.attachments.size === 0) {
-      message.client.channels.fetch("669192672132595716").then(salondm => salondm.send(`${msgcontent} \n\n c'est ${message.author.tag} qui a écrit cette idée !`)).then(message.channel.send("Votre idée a bien été reçu !"));
+      message.client.channels.fetch("669192672132595716").then(salondm => salondm.send(`${msgcontent} \n\n c'est \`${message.author.tag}\` qui a écrit cette idée !`)).then(message.channel.send("Votre idée a bien été reçu !"));
     } else {
       message.client.channels.fetch("669192672132595716").then(salondm => salondm.send(`${msgcontent} \n\n c'est ${message.author.tag} qui a écrit cette idée ! ${message.attachments.first().url}`)).then(message.channel.send("Votre idée a bien été reçu !"));
     }
--- commands/moderation/utilitaire/report.js
+++ commands/moderation/utilitaire/report.js
@@ -19,7 +19,7 @@ class reportcommands extends Command {
     if (message.channel.type === "dm") return message.reply("Tu ne peux pas faire de report en mp même si ça aurait été bien !");
     const msgcontent = args.messagecontent;
     if (message.attachments.size === 0) {
-      message.client.channels.fetch("669192672132595716").then(salondm => salondm.send(`${msgcontent} \n\n c'est ${message.author.tag} qui a écrit ce rapport de bug !`)).then(message.channel.send("Votre report de bug a bien été reçu !"));
+      message.client.channels.fetch("669192672132595716").then(salondm => salondm.send(`${msgcontent} \n\n c'est \`${message.author.tag}\` qui a écrit ce rapport de bug !`)).then(message.channel.send("Votre report de bug a bien été reçu !"));
     } else {
       message.client.channels.fetch("669192672132595716").then(salondm => salondm.send(`${msgcontent} \n\n c'est ${message.author.tag} qui a écrit ce rapport de bug ! ${message.attachments.first().url}`)).then(message.channel.send("Votre report de bug a bien été reçu !"));
     }
