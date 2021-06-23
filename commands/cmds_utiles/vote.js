fd0e1273402caeff6231df8b5d8e8b6be62af37f
Ludo-code <56892223+Ludo-code@users.noreply.github.com>
2021-06-24 00:48:43 +0200

    update de vote

--- commands/cmds_utiles/vote.js
+++ commands/cmds_utiles/vote.js
@@ -11,7 +11,7 @@ class votecommands extends Command {
 
   async exec(message) {
   if (message.channel.type === "dm") return message.reply("Oui je bloque les informations du bot en MP et alors c'est pas de ma faute c'est mon maitre ! :joy:");
-  message.channel.send("Pour votez pour le bot fonce sur ce lien : https://bladebotlist.xyz/bot/636098912859717634 \n \n cela me permet de lui faire gagnez en visibiliter et par conséquent l'améliorer.");
+  message.channel.send("Pour votez pour le bot fonce sur ce lien : <https://bladebotlist.xyz/bot/636098912859717634> \n \n cela me permet de lui faire gagnez en visibiliter et par conséquent l'améliorer :wink:");
   }
 }
 module.exports = votecommands;
