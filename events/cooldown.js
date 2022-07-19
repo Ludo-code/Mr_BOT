b736688f011d59c19ab55243ab07d40f60d458e4
Ludo-Code <lasagne.berthet@gmail.com>
2022-07-19 23:58:21 +0200

    correction du temps du cooldown

--- events/cooldown.js
+++ events/cooldown.js
@@ -1,20 +1,24 @@
-const { Event } = require("sheweny");
+const { Event, Command } = require("sheweny");
 
 class eventcooldown extends Event {
-  constructor() {
-    super("cooldown", {
-      emitter: "commandHandler",
-      event: "cooldownlimit",
+  constructor(client) {
+    super(client, "cooldownLimit", {
+      description: "Limite cooldown",
+      once: false,
     });
   }
 
-  exec(message, Command, number) {
+  exec(message, Command, time) {
     console.log("cooldown");
-    const secondes = Math.floor(number / 1000) % 60;
 
-    const minutes = Math.floor(number / 60000) % 60;
+    const base = time;
+    const heures = Math.floor(base / 3600);
+    const resteheures = Math.floor(base % 3600);
+    const minutes = Math.floor(resteheures / 60);
+    const secondes = Math.floor(resteheures % 60);
+
     message.channel.send(
-      `La commande \`${Command}\` est en cooldown merci d'attendre **${minutes} minutes, ${secondes} secondes.**`
+      `La commande \`${Command}\` est en cooldown merci d'attendre **${heures} heures, ${minutes} minutes, ${secondes} secondes.**`
     );
   }
 }
