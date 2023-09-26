6b28a8b9d01ed20823e9e966c8f233b4aebd48c6
Ludo-code <56892223+Ludo-code@users.noreply.github.com>
2023-09-26 18:40:27 +0200

    Changement de l'affichage de la version
    Le package.json est maintenant utiliser pour afficher la variable..

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,16 +1,16 @@
 import {ActivityType, Events} from 'discord.js';
+import pkgjson from '../../package.json' assert { type: 'json' };
 
 export const event = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
 		console.log(`Prêt ? Le bot est connecté en tant que ${client.user.tag} ( ${client.user.id} )`);
-		const version = require('../../package.json')
 		const activitees = [
 		  " m*aide",
 		  " m*aide_nsfw",
 		  `${client.guilds.cache.size} serveurs !`,
-		  `la version ${version}...`,
+		  `la version ${pkgjson.version}...`,
 		];
 		let nombre = 0;
 		setInterval(() => {
