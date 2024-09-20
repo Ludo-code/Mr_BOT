b6d254b39a3f8b97222a2a89cbe3c29cdef1e6f4
Ludovic <56892223+Ludo-code@users.noreply.github.com>
2024-09-20 18:07:35 +0200

    Finish config file

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export default {
     ownerId: '268432158262165504',
     clientId: '636098912859717634',
     guildId: '797562340807409704',
-    voteURL: 'https://example.com',
+    voteURL: 'https://top.gg/bot/636098912859717634?s=0418acfa64d7e',
     websiteURL: 'https://mr-bot.tech-ludo.fr',
     githubURL: 'https://github.com/Ludo-code/Mr_BOT',
     supportServerURL: 'https://discord.com/invite/4z5zvdae7r',
