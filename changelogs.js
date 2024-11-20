da7d3e11fab9b97ff1411281884aca8aecd55ad8
Ludovic <56892223+Ludo-code@users.noreply.github.com>
2024-11-20 21:48:15 +0100

    New changelog

--- changelogs.js
+++ changelogs.js
@@ -1,5 +1,8 @@
 export default [
-    '```Nouveauté des mises à jour : \n\n- le 08/09/2023 Reprise du développement, suppression des vieilles commandes abandon de Sheweny et Discord-Akairo.```',
-    '```Nouveauté des mises à jour : \n\n- le 12/09/2023 Ajout de commande NSFW supplémentaire.```',
-    '```Nouveauté des mises à jour : \n\n- le 26/09/2023 Correction de divers bug dont 1 critique.```'
+    "Nouveauté des mises à jour : \n\n- le 08/09/2023 Reprise du développement, suppression des vieilles commandes abandon de Sheweny et Discord-Akairo.",
+    "Nouveauté des mises à jour : \n\n- le 12/09/2023 Ajout de commande NSFW supplémentaire.",
+    "Nouveauté des mises à jour : \n\n- le 26/09/2023 Correction de divers bugs dont 1 critique.",
+    "Nouveauté des mises à jour : \n\n- le 21/10/2024 Correction de divers bugs, Uniformisation des quotes",
+    "Nouveauté des mises à jour : \n\n- le 20/11/2024 Nouvelles aides",
+
 ];
