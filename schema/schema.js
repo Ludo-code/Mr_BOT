ea9fcaa21496bebfeb07cb4e17bac550bd59579d
Ludovic <56892223+Ludo-code@users.noreply.github.com>
2025-02-26 17:16:20 +0100

    standardjs + new bdd shem

--- schema/schema.js
+++ schema/schema.js
@@ -1,32 +1,49 @@
-import { DataTypes, Sequelize } from "sequelize";
-import "dotenv/config";
+import { DataTypes, Sequelize } from 'sequelize'
+import 'dotenv/config'
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    logging: () => {}
-});
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  logging: () => {}
+})
 
 try {
-    await sequelize.authenticate();
-    console.log("Connection à la bdd réussi !");
+  await sequelize.authenticate()
+  console.log('Connection à la bdd réussi !')
 } catch (error) {
-    console.log("Connection à la base de donné en échec");
+  console.log('Connection à la base de donné en échec')
 }
 
-const Guild = sequelize.define("Guild", {
-    guildId: DataTypes.STRING,
-    ticketCategoryChannelID: DataTypes.STRING,
-    nsfwEnabled: DataTypes.BOOLEAN,
-});
+const Guild = sequelize.define('Guild', {
+  guildId: {
+    type: DataTypes.STRING
+  },
+  ticketCategoryChannelID: {
+    type: DataTypes.STRING
+  },
+  nsfwEnabled: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  defaultLanguage: {
+    type: DataTypes.STRING,
+    defaultValue: 'FR'
+  }
+})
 
-const Ticket = sequelize.define("Ticket", {
-    userId: DataTypes.STRING,
-    channelId: DataTypes.STRING,
-    guildId: DataTypes.STRING,
-});
+const Ticket = sequelize.define('Ticket', {
+  userId: {
+    type: DataTypes.STRING
+  },
+  channelId: {
+    type: DataTypes.STRING
+  },
+  guildId: {
+    type: DataTypes.STRING
+  }
+})
 
-await sequelize.sync();
+await sequelize.sync()
 
-export { Guild, Ticket };
+export { Guild, Ticket }
