3e801c5dcb79a6617857c43b49e4c87f334ce995
Ludovic <56892223+Ludo-code@users.noreply.github.com>
2025-07-24 23:35:43 +0200

    Up tenor api to v2

--- commands/roleplay/good_night.js
+++ commands/roleplay/good_night.js
@@ -10,15 +10,15 @@ export const command = {
     clientpermissions: [PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.EmbedLinks],
     async execute(message, args) {
         try {
-            let res = await (await fetch(`https://g.tenor.com/v1/random?id=8776030&key=${process.env.TENOR_API_KEY}&limit=1&q=good+night+anime`))?.json();
-            if (!res?.results || !res.results[0]?.media || !res.results[0]?.media[0]?.gif?.url) return await message.reply("Impossible d'obtenir l'image");
+            let res = await (await fetch(`https://tenor.googleapis.com/v2/search?key=${process.env.TENOR_API_KEY}&client_key=${process.env.TENOR_API_KEY_NAME}&random=true&limit=1&q=good+night+anime`))?.json();
+            if (!res?.results || !res.results[0]?.media_formats || !res.results[0]?.media_formats?.gif?.url) return await message.reply("Impossible d'obtenir l'image");
 
             let mentionedmember = message.mentions.members.first();
 
             let embed = new EmbedBuilder()
                 .setTitle(`Bonne Nuit, ${mentionedmember ? (mentionedmember.nickname || mentionedmember.user.username) : (message.member.nickname || message.author.username)}`)
                 .setColor("Random")
-                .setImage(res.results[0]?.media[0]?.gif?.url);
+                .setImage(res.results[0]?.media_formats?.gif?.url);
 
             await message.reply({
                 embeds: [embed],
