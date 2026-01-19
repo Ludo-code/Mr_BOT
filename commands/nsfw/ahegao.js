26509e2808b95f784a0734c8d1f572194d3ae135
Ludo-code <56892223+Ludo-code@users.noreply.github.com>
2026-01-19 18:43:54 +0100

    move from tenor to klipy

--- commands/nsfw/ahegao.js
+++ commands/nsfw/ahegao.js
@@ -10,13 +10,17 @@ export const command = {
     clientpermissions: [PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.EmbedLinks],
     async execute(message, args) {
         try {
-            let res = await (await fetch(`https://tenor.googleapis.com/v2/search?key=${process.env.TENOR_API_KEY}&client_key=${process.env.TENOR_API_KEY_NAME}&random=true&limit=1&q=ahegao`))?.json();
-            if (!res?.results || !res.results[0]?.media_formats || !res.results[0]?.media_formats?.gif?.url) return await message.reply("Impossible d'obtenir l'image");
+            let res = await (await fetch(`https://api.klipy.com/api/v1/${process.env.KLIPY_API_KEY}/gifs/search?customer_id=${process.env.KLIPY_API_KEY_NAME}&limit=24&q=ahegao`))?.json();
+            if (!res?.data?.data || !res.data.data[0]?.file) return await message.reply("Impossible d'obtenir l'image");
+
+            const randomizeresults = Math.floor(Math.random() * res.data.data.length);
+            let gifselector = res.data.data[randomizeresults]?.file?.hd?.gif?.url || res.data.data[randomizeresults]?.file?.md?.gif?.url || res.data.data[randomizeresults]?.file?.sm?.gif?.url || res.data.data[randomizeresults]?.file?.xs?.gif?.url;
 
             let embed = new EmbedBuilder()
                 .setTitle(`Une image d'ahegao pour toi, ${message.member.nickname || message.author.username}`)
                 .setColor("Random")
-                .setImage(res.results[0]?.media_formats?.gif?.url);
+                .setImage(gifselector)
+                .setFooter({text: "Powered by KLIPY"});
 
             await message.reply({
                 embeds: [embed],
--- commands/roleplay/good_night.js
+++ commands/roleplay/good_night.js
@@ -10,15 +10,19 @@ export const command = {
     clientpermissions: [PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.EmbedLinks],
     async execute(message, args) {
         try {
-            let res = await (await fetch(`https://tenor.googleapis.com/v2/search?key=${process.env.TENOR_API_KEY}&client_key=${process.env.TENOR_API_KEY_NAME}&random=true&limit=1&q=good+night+anime`))?.json();
-            if (!res?.results || !res.results[0]?.media_formats || !res.results[0]?.media_formats?.gif?.url) return await message.reply("Impossible d'obtenir l'image");
+            let res = await (await fetch(`https://api.klipy.com/api/v1/${process.env.KLIPY_API_KEY}/gifs/search?customer_id=${process.env.KLIPY_API_KEY_NAME}&limit=24&q=good+night+anime`))?.json();
+            if (!res?.data?.data || !res.data.data[0]?.file) return await message.reply("Impossible d'obtenir l'image");
+
+            const randomizeresults = Math.floor(Math.random() * res.data.data.length);
+            let gifselector = res.data.data[randomizeresults]?.file?.hd?.gif?.url || res.data.data[randomizeresults]?.file?.md?.gif?.url || res.data.data[randomizeresults]?.file?.sm?.gif?.url || res.data.data[randomizeresults]?.file?.xs?.gif?.url;
 
             let mentionedmember = message.mentions.members.first();
 
             let embed = new EmbedBuilder()
                 .setTitle(`Bonne Nuit, ${mentionedmember ? (mentionedmember.nickname || mentionedmember.user.username) : (message.member.nickname || message.author.username)}`)
                 .setColor("Random")
-                .setImage(res.results[0]?.media_formats?.gif?.url);
+                .setImage(gifselector)
+                .setFooter({text: "Powered by KLIPY"});
 
             await message.reply({
                 embeds: [embed],
